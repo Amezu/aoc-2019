214fdf48fd8c39b03c564454bebb8fe35bdb96b2
Julia Zarzycka <36897051+Amezu@users.noreply.github.com>
2019-12-02 19:45:01 +0100

    Update Day 2 Part 1 solution

--- day-2.py
+++ day-2.py
@@ -14,7 +14,7 @@ def runProgram(program):
                 program[program[i+2]]
         else:
             raise Exception("Invalid opcode")
-    return program
+    return program[0]
 
 
 testPrograms = [[1, 0, 0, 0, 99],
@@ -26,16 +26,22 @@ testOutputs = [[2, 0, 0, 0, 99],
                [2, 4, 4, 5, 99, 9801],
                [30, 1, 1, 4, 2, 5, 6, 0, 99]]
 
-if all(map(lambda x: runProgram(x[0]) == x[1],
+for program in testPrograms:
+    runProgram(program)
+
+if all(map(lambda x: x[0] == x[1],
            zip(testPrograms, testOutputs))):
     print("Tests passed")
+else:
+    print("Tests not passed")
+
 
 file = open("input.txt")
 program = file.read().split(',')
 file.close()
 
 program = [int(i) for i in program]
-program[1] = 12
-program[2] = 2
-runProgram(program)
-print(program[0])
+program_copy = program.copy()
+program_copy[1] = 12
+program_copy[2] = 2
+print("Part 1:", runProgram(program_copy))
