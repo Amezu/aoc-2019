9c27234774ea1b5276ddc188f237ad956ce5c1c8
Julia Zarzycka <36897051+Amezu@users.noreply.github.com>
2019-12-05 23:40:24 +0100

    Refactor Day 4

--- day-4.py
+++ day-4.py
@@ -1,29 +1,28 @@
 def checkCriteria(number):
+    count = 0
     containsDouble = False
     digit = number % 10
     number //= 10
-    for i in range(5):
+    for i in range(6):
         nextDigit = digit
         digit = number % 10
         number //= 10
         if digit > nextDigit:
             return False
         elif digit == nextDigit:
-            containsDouble = True
+            count += 1
+        elif digit < nextDigit:
+            if count == 1:
+                containsDouble = True
+            count = 0
     return containsDouble
 
 
-testData = [[111111, True], [223450, False], [123789, False]]
+testData = [[112233, True], [123444, False], [111122, True], [335666, True]]
 hasTestPassed = all(map(lambda i: checkCriteria(i[0]) == i[1], testData))
-print("Test " + "passed" if hasTestPassed else "not passed")
-
+print("Test " + ("passed" if hasTestPassed else "not passed"))
 
 min = 264793
 max = 803935
 
-sum = 0
-for i in range(min, max + 1):
-    if (checkCriteria(i)):
-        sum += 1
-
-print(sum)
+print(sum(1 for i in range(min, max+1) if checkCriteria(i)))
